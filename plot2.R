065750aef19abb8c3ec7b63da0568446b5a4ac15
szetov <szetov@gmail.com>
2017-10-20 23:04:20 +0800

    complete plot

---
+++ plot2.R
@@ -0,0 +1,20 @@
+library(lubridate)
+#download data
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+              destfile = "temp.zip")
+unzip(zipfile = "temp.zip")
+
+#load data and clean up
+power <- read.table("household_power_consumption.txt", header = TRUE, nrows = 69516,
+                    sep = ";", colClasses = c(rep("character",2), rep("numeric", 7)),
+                    na.strings = "?")
+power$datetime <- strptime(paste(power$Date, power$Time), format = "%d/%m/%Y %H:%M:%S")
+#optimize memory footprint by removing unused data
+power <- subset(power, power$datetime >= strptime("2007-02-01", format = "%Y-%m-%d"))
+
+#generate plot
+png(file = "plot2.png", bg="transparent", width = 480, height = 480)
+with(power, plot(datetime, Global_active_power, type="n", xlab = "",
+                 ylab = "Global active power (kilowatts)"))
+with(power, lines(datetime, Global_active_power))
+dev.off()
